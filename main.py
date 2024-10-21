9c0750ea3642ff459e1fda07abe6f7b742ba6acb
caglanakpinar <cakpinar23@gmail.com>
2024-10-21 11:30:49 +0200

    updating readme file and example of training and tuning process at main.py

--- main.py
+++ main.py
@@ -6,3 +6,25 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 if __name__ == "__main__":
     cli()
+
+
+# train model
+"""
+poetry run python main.py \
+model train \
+--training_class dcgan.DCGAN \
+--trainer_config_path configs/params.yaml \
+--data_access_clas utils.CapturingImages \
+--continuous_training True \
+"""
+# hyperparameter tuning
+# train model
+"""
+poetry run python main.py \
+model train \
+--tuning_class dcgan.HyperDCGAN \
+--trainer_config_path configs/params.yaml \
+--hyperparameter_config_path configs/hyper_params.yaml \
+--data_access_clas utils.CapturingImages \
+--continuous_training True
+"""
